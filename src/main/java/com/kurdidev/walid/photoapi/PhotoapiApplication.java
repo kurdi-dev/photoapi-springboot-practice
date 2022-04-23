70165a0f714eb250290c3f377d235de6eca606c6
Walid <walid.r.rashed@gmail.com>
2022-04-23 16:57:32 +0300

    chore: initial commit

---
+++ src/main/java/com/kurdidev/walid/photoapi/PhotoapiApplication.java
@@ -0,0 +1,13 @@
+package com.kurdidev.walid.photoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurdidev/walid/photoapi/PhotoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurdidev.walid.photoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
